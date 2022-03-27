a498e19043066dda5c7f82c4ba29bdaccdd08960
BrunoBorelliPro <brunoborellipro@gmail.com>
2022-03-27 19:55:01 -0300

    Utilizando enums

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+from email import message
 from enum import Enum
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    bruno0 = "bruno0"
+    bruno1= "bruno1"
+    bruno2= "bruno2"
 
 app = FastAPI()
 
@@ -10,3 +16,7 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    return {"model_name": model_name, "message": f"Vai {model_name}! Você consegue!"}
