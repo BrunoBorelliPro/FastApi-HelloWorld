8964a240375a82ed2e56c9bff25106cd9622e875
BrunoBorelliPro <brunoborellipro@gmail.com>
2022-03-27 19:42:10 -0300

    Aprendendo FastAPI - Commit inicial

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from enum import Enum
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "hello world!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
